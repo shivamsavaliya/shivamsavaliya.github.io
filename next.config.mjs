1cc8691723de8c222c77986f98ed3a352be63743
shivamsavaliya <savaliyashivam2002@gmail.com>
2026-04-15 18:42:51 -0500

    add config code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio_react",
+  assetPrefix: "/portfolio_react/",
 };
 
 export default nextConfig;
